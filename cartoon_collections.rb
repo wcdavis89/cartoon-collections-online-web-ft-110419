020a3aba07ffce77b9931beebab2d305e72842fb
Whittney Davis <wcdavis89@yahoo.com>
2019-11-13 23:27:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   found_cheese=[]
   cheese_types = ["cheddar", "gouda", "camembert"]
  if  array.include?(cheese_types) do |item|
-   p item
+   p found_cheese << item
  end
   else 
     puts found_cheese
