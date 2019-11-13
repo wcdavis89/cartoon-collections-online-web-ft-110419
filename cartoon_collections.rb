ae765af4b0c51c4958f2a61d2fdb5aaac2091a33
Whittney Davis <wcdavis89@yahoo.com>
2019-11-13 23:37:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each do |name|
+    puts "#{array.index(name)+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |call|
+    call << "!"
+    call.capitalize
+  end
+end
+def long_planeteer_calls(array)
+ array.select do |word|
+return  word.size<4
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+ array.find do |cheese|
+   cheese_types.include?(cheese)
+  end
 end
